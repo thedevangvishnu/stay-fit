5a77107e88b81cbd982ed85553d100e0214c5642
Devang Vishnu <thedevangvishnu@gmail.com>
2024-02-05 11:34:35 +0530

    Add cors package and configure app to response to localhost:3000

--- stay-fit-api/src/app.js
+++ stay-fit-api/src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const { exercisesRouter } = require("./routes/exercises/exercises.routes");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 app.use("/exercises", exercisesRouter);
